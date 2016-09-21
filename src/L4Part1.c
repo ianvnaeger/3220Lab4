a99833d5443af7bf0429ca929e2e468b7e2010cf
Ian Naeger <ianvnaeger@gmail.com>
2016-09-21 16:44:03 -0500

    Finished Part 1, but seg faults

---
+++ src/L4Part1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : 3220Lab4.c
+ Author      : Ian Naeger
+ Version     :
+ Copyright   : N/A
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Lab 4"); /* prints Lab 4 */
+	return EXIT_SUCCESS;
+}
